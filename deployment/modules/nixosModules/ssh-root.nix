9302a35a1749d74094f321b6e8b1088f9388ed23
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-06-08 11:08:35 +0200

    fix: remove failed dep

--- deployment/modules/nixosModules/ssh-root.nix
+++ deployment/modules/nixosModules/ssh-root.nix
@@ -6,7 +6,7 @@
     };
 
     users.users.root.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHdCri0n+rL2Ziajo1qRNDWXS0G/Jh3hZdj8waE9uhH" # Bootstrap key
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5uAZGPf8TJI9N5uyVAHxG8AsWu6AJmdkWGMkhpriDa" # Bootstrap key
     ];
   };
 }
